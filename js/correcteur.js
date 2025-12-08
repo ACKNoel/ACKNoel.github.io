ea643c786ac7e264cbfaffed08b70ee4e9e0c00e
Paul-D <john@example.com>
2025-12-08 15:01:04 -0500

    Ajouté une fonction correcteur au script

---
+++ js/correcteur.js
@@ -0,0 +1,47 @@
+let pays = 0;
+let indice = 0;
+let score = 0;
+
+let tblPays = [
+    "Canada",
+    "France",
+    "Russie",
+    "Mexique",
+    "Japon",
+    "Angleterre",
+    "Thailande"
+];
+
+let tblIndices = [
+    ["indice canada 1", "indice canada 2", "indice canada 3", "indice canada 4", "indice canada 5"]
+    ["indice france 1", "indice france 2", "indice france 3", "indice france 4", "indice france 5"]
+    ["indice russie 1", "indice russie 2", "indice russie 3", "indice russie 4", "indice russie 5"]
+    ["indice mexique 1", "indice mexique 2", "indice mexique 3", "indice mexique 4", "indice mexique 5"]
+    ["indice japon 1", "indice japon 2", "indice japon 3", "indice japon 4", "indice japon 5"]
+    ["indice angleterre 1", "indice angleterre 2", "indice angleterre 3", "indice angleterre 4", "indice angleterre 5"]
+    ["indice thailande 1", "indice thailande 2", "indice thailande 3", "indice thailande 4", "indice thailande 5"]
+];
+
+//Source Math.random https://www.w3schools.com/js/js_random.asp
+function pigePays() {
+    pays = Math.floor(Math.random() * (tblPays.length))
+    return pays;
+}
+
+function correcteur() {
+    let message;
+    let boiteAffiche = document.getElementById("divAffiche");
+    let reponse = document.getElementById("reponse").value;
+
+    if (reponse.toLowerCase() == tblPays[pays].toLowerCase()) {
+        score = score + (100 - indice*20);
+        message = "Vous avez réussi!<br><br>Le pays était " + tblPays[pays] + ".";
+        indice = 0;
+    }
+    else {
+        indice++;
+        message = "Incorrect. Voici le prochain indice:<br>" + tblIndices[indice];
+    }
+
+    boiteAffiche.innerHTML = message;
+}
