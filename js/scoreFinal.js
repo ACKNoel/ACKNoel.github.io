85fce4a96dc8494ff54129420d1374629ff5144d
Paul-D <john@example.com>
2025-12-11 09:24:41 -0500

    ajout scorefinal js

--- js/scoreFinal.js
+++ js/scoreFinal.js
@@ -1,6 +1,7 @@
 //DÉCLARATION DES VARIABLES:
 
 let score = localStorage.getItem("score");
+let nom = localStorage.getItem("nomUtilisateur");
 let boiteScoreFinal = document.getElementById("boiteScoreFinal");
 let messageScoreFinal = "<p>Score: " + score + "</p>";
 
