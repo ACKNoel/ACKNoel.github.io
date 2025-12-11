f98ef789225bc219406cac24001a3098d301b6c3
Paul-D <Paul-D>
2025-12-11 10:30:49 -0500

    Ajoute de code Corie

--- js/scoreFinal.js
+++ js/scoreFinal.js
@@ -3,8 +3,11 @@
 let score = localStorage.getItem("score");
 let nom = localStorage.getItem("nomUtilisateur");
 let boiteScoreFinal = document.getElementById("boiteScoreFinal");
+let boiteNomUtilisateur = document.getElementById("boiteNomUtilisateur");
 let messageScoreFinal = "<p>Score: " + score + "</p>";
+let messageNomUtilisateur = "<h1>Score finale de "+ nom +":<h1>";
 
 //CODE PRINCIPAL:
 
 boiteScoreFinal.innerHTML = messageScoreFinal;
+boiteNomUtilisateur.innerHTML = messageNomUtilisateur; 
