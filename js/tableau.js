9f62e47c8280976d9a616ee8bf33231e559abc45
Paul-D <Paul-D>
2025-12-15 11:55:42 -0500

    Trouve comment insérer des photo en tableau js Corie

---
+++ js/tableau.js
@@ -0,0 +1,18 @@
+//apprendre comment insérer des photo an js
+//tableau à 2 dimensions avec toutes les indices dans le même ordre que les pays
+let tblIndices = [
+    //Canada:
+    ["Ho ho ho! Je suis dans un pays avec la plus grande bordure de la mer!", "Il y a plus de lacs dans ce pays que dans tous les autres pays combinés.", "Je suis dans un pays bilingue!", "Ho ho ho! Je me retrouve dans un très grand pays au nord!", <img src="image/Canada.png" width="75%"/>],
+    //France:
+    ["Ho ho ho! Je suis dans un pays qui est donné le pseudo-nom “l\’Hexagone” à cause de sa forme!", "Wow! Ce pays a beaucoup de plaines très belles au nord et des jolies montagnes à l\’est et au sud.", "Il y a tellement d\’autres visiteurs ici! En fait, ce pays est le plus visité mondialement!", "La culture ici est centrée sur la nourriture, les arts et la mode!", <img src="image/France.png" width="75%"/>],
+    //Russie:
+    ["Ho ho ho! Le pays où je me retrouve a le plus de forêts dans le monde!", "Hm, les lettres d\’ici semblent être de l\’alphabet cyrillique!", "Le pays où je suis se retrouve sur 2 continents!", "Ce pays est tellement gros, et il fait aussi très froid ici!", <img src="image/Russie.png" width="75%"/>],
+    //Mexique:
+    ["Ho ho ho! Je visite un pays avec la plus grande pyramide en termes de volume!", "Je suis dans un pays où vivaient des grandes civilisations anciennes!", "Wow, c\’est ici où le chocolat est originaire.", "Il y a tellement de festivals animés et colorés avec de la bonne nourriture et musique dans ce pays!", <img src="image/Canada.png" width="75%"/>],
+    //Japon:
+    ["Ho ho ho! Je suis dans le pays avec le pourcentage le plus élevé de tsunamis au monde.", "Ho ho ho! Les traditions ici sont une combinaison de shintō et bouddhisme.", "Les gens dans ce pays ont une des espérances de vie la plus élevée mondialement!", "Ce pays est connu comme étant très futuriste mais aussi traditionnel en même temps!", <img src="image/Japon.png" width="75%"/>],
+    //Angleterre:
+    ["indice angleterre 1", "indice angleterre 2", "indice angleterre 3", "indice angleterre 4", "indice angleterre 5"],
+    //Thailande:
+    ["indice thailande 1", "indice thailande 2", "indice thailande 3", "indice thailande 4", "indice thailande 5"]
+];
