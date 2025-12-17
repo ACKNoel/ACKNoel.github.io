d9e29b597575197677d89166a24ed546c38bd07b
Paul-D <john@example.com>
2025-12-17 12:43:19 -0500

    popupRetry

--- js/chercheurNom.js
+++ js/chercheurNom.js
@@ -1,20 +1,21 @@
 //FONCTIONS:
 
+const invalide = document.getElementById("popupRetry")
+function ouvrirPopup() {
+    popup.style.display = "block";
+}
+// Function to close the popup
+function fermerPopup() {
+    popup.style.display = "none";
+}
+    
 //fonction qui cherche le nom d'utilisateur inscrit
 function chercheNomUtilisateur() {
     let entreeNom = document.getElementById("nomUtilisateur").value;
     while (entreeNom.length == 0){
-        let invalide = 
-        invalide.innerHTML = <div id="popupAide" class="popup-overlay">
-                            <div class="popup-contenu">
-                                <h2>Trouve le Père Noël</h2>
-                                <button id="fermerPopupBtn" class="buttonStyle">Fermer</button>
-                            </div>
-                        </div>
-        alert("Nom d'utilisateur invalide. Réessayer.")
-        location.href = "index.html"
+        ouvrirPopup();
         entreeNom = document.getElementById("nomUtilisateur").value;
     }
     localStorage.setItem("nomUtilisateur", entreeNom);
-       location.href = "trouveperenoel.html";
+    location.href = "trouveperenoel.html";
 }
