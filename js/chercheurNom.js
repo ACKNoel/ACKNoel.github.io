404a4c2321cc864d4ca4da397a12b04109dc66bd
Paul-D <john@example.com>
2025-12-17 10:30:46 -0500

    commit

--- js/chercheurNom.js
+++ js/chercheurNom.js
@@ -4,7 +4,15 @@
 function chercheNomUtilisateur() {
     let entreeNom = document.getElementById("nomUtilisateur").value;
     while (entreeNom.length == 0){
+        let invalide = 
+        invalide.innerHTML = <div id="popupAide" class="popup-overlay">
+                            <div class="popup-contenu">
+                                <h2>Trouve le Père Noël</h2>
+                                <button id="fermerPopupBtn" class="buttonStyle">Fermer</button>
+                            </div>
+                        </div>
         alert("Nom d'utilisateur invalide. Réessayer.")
+        location.href = "index.html"
         entreeNom = document.getElementById("nomUtilisateur").value;
     }
     localStorage.setItem("nomUtilisateur", entreeNom);
